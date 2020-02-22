dc005efac561673fe28c14e29787c562be7bcdf6
latesun <latesun.lee@bindo.com>
2020-02-22 14:08:14 +0800

    Fix mongo sort query.

--- master/LogMgr.go
+++ master/LogMgr.go
@@ -37,7 +37,6 @@ func InitLogMgr() (err error) {
 func (logMgr *LogMgr) ListLog(name string, skip int, limit int) (logArr []*common.JobLog, err error) {
 	var (
 		filter *common.JobLogFilter
-		// logSort *common.SortLogByStartTime
 		cur    *mongo.Cursor
 		jobLog *common.JobLog
 	)
@@ -49,14 +48,11 @@ func (logMgr *LogMgr) ListLog(name string, skip int, limit int) (logArr []*commo
 	filter = &common.JobLogFilter{JobName: name}
 
 	// 按照任务开始时间倒排
-	// logSort = &common.SortLogByStartTime{SortOrder: -1}
+	findOption := options.Find()
+	findOption.SetSort(common.SortLogByStartTime{SortOrder: -1})
 
-	// 查询
-	// if cursor, err = logMgr.logCollection.Find(context.TODO(), filter, findopt.Sort(logSort), findopt.Skip(int64(skip)), findopt.Limit(int64(limit))); err != nil {
-	// 	return
-	// }
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	cur, err = logMgr.logCollection.Find(ctx, filter)
+	cur, err = logMgr.logCollection.Find(ctx, filter, findOption)
 	if err != nil {
 		return
 	}
